5a77d55085846c79224a8920e80ec1db535ace60
King-Skymoon <King-Skymoon@users.noreply.github.com>
2017-09-17 18:53:36 -0400

    test
    test

--- files/code/javascript/usertime/usertime.js
+++ files/code/javascript/usertime/usertime.js
@@ -1,1 +1,15 @@
-console.log('test');
+$(document).ready(function() {
+        if("<?php echo $timezone; ?>".length==0){
+            var visitortime = new Date();
+            var visitortimezone = "GMT " + -visitortime.getTimezoneOffset()/60;
+			console.log(visitortimezone);
+            $.ajax({
+                type: "GET",
+                url: "timezone.php",
+                data: 'time='+ visitortimezone,
+                success: function(){
+                    location.reload();
+                }
+            });
+        }
+});
