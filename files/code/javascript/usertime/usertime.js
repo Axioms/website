5422718e7d4fa1f343c7141d916c40c605b36d00
Axioms <15842624+Axioms@users.noreply.github.com>
2017-09-17 19:05:23 -0400

    s
    s

--- files/code/javascript/usertime/usertime.js
+++ files/code/javascript/usertime/usertime.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
         if("<?php echo $timezone; ?>".length==0){
             var visitortime = new Date();
-            var visitortimezone = "GMT " + -visitortime.getTimezoneOffset()/60;
-			console.log(char(visitortimezone));
+            var visitortimezone = visitortime.getHours();
+			console.log(visitortimezone);
             $.ajax({
                 type: "GET",
-                url: "timezone.php",
+                url: "https://bits.ddnsking.com/filemanager/customizables/config.php",
                 data: 'time='+ visitortimezone,
                 success: function(){
                     location.reload();
