a79da00dd5643e1db213ae352cc9dc0105a224d1
Axioms <15842624+Axioms@users.noreply.github.com>
2018-07-09 20:41:26 -0400

    animations for desktop

--- res/bootstrap-sidebar/bootstrap-sidebar.js
+++ res/bootstrap-sidebar/bootstrap-sidebar.js
@@ -14,3 +14,27 @@ function smallWindowToggle() {
         $(".sb-data").animate({height: "0%"},1000,function(){});
     }
 }
+function largeWindowToggle() {
+    if($(".sb-link").css("height") == "0px") {
+        $(".sb-link").animate({width: "66%", height: "100%"}, 1000, function(){});
+        $("#sb-main").animate({width: "20%"}, 1000, function(){});
+        $("#win-toggle-a").prop("title", "Collapse");
+        flipArrow();
+    }
+    else {
+        $(".sb-link").animate({width: "0px", height: "0px"}, 1000, function(){});
+        $("#sb-main").animate({width: "5%"}, 1000, function(){});
+        $("#win-toggle-a").prop("title", "Expand");
+        flipArrow();
+    }
+}
+function flipArrow() {
+    if($("#arrow").hasClass("fa-arrow-left")) {
+        $("#arrow").removeClass("fa-arrow-left");
+        $("#arrow").addClass("fa-arrow-right");
+    }
+    else {
+        $("#arrow").removeClass("fa-arrow-right");
+        $("#arrow").addClass("fa-arrow-left");
+    }
+}
