09e0d04ef1d401c657a3bdc6b55eaf6a14462391
Axiom <karan15547@gmail.com>
2018-07-14 21:02:06 -0400

    fixed url-wrapper

--- res/index/url-wrapper.js
+++ res/index/url-wrapper.js
@@ -1,12 +1,15 @@
 "use strict";
 var siteHistory = [];
+var afterPopstate = false;
 function changePage() {
     if(window.location.hash != '') {
         let pageHash = window.location.hash.substr(1);
         //needed to replace the iframe because popstate was implicitly selecting it
         $("#page-content-wrapper").replaceWith(('<iframe name="frame" id="page-content-wrapper" frameBorder="0" src="' + pageHash +'">'));
-        siteHistory.push(pageHash);
-        window.history.replaceState(siteHistory, "Axiom\'s Website", "/#" + pageHash);
+        if(!afterPopstate) {
+            siteHistory.push(pageHash);
+            window.history.replaceState(siteHistory, "Axiom\'s Website", "/#" + pageHash);
+        }
     }
 }
 $(window).on('popstate', function (e) {
@@ -17,6 +20,6 @@ $(window).on('popstate', function (e) {
         console.table(siteHistory);
         siteHistory.pop();
         window.history.replaceState(siteHistory, "Axiom\'s Website", "/#" + pageHash);
-        changePage();
+        afterPopstate = true;
     }
 });
