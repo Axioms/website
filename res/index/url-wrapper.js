02d577f320e552c5d3f546490d7a150bed490207
Axiom <karan15547@gmail.com>
2018-07-18 01:07:44 -0400

    seamless iframes are a pain

--- res/index/url-wrapper.js
+++ res/index/url-wrapper.js
@@ -15,7 +15,6 @@ function changePage() {
 $(window).on('popstate', function (e) {
     var state = e.originalEvent.state;
     if (state !== null) {
-        //load content with ajax
         let pageHash = window.location.hash.substr(1);
         console.table(siteHistory);
         siteHistory.pop();
@@ -23,21 +22,3 @@ $(window).on('popstate', function (e) {
         afterPopstate = true;
     }
 });
-function getDocHeight(doc) {
-    doc = doc || document;
-    // stackoverflow.com/questions/1145850/
-    var body = doc.body, html = doc.documentElement;
-    var height = Math.max( body.scrollHeight, body.offsetHeight, 
-        html.clientHeight, html.scrollHeight, html.offsetHeight );
-    return height;
-}
-function setIframeHeight(id) {
-    var ifrm = document.getElementById(id);
-    var doc = ifrm.contentDocument? ifrm.contentDocument: 
-        ifrm.contentWindow.document;
-    ifrm.style.visibility = 'hidden';
-    ifrm.style.height = "10px"; // reset to minimal height ...
-    // IE opt. for bing/msn needs a bit added or scrollbar appears
-    ifrm.style.height = getDocHeight( doc ) + 4 + "px";
-    ifrm.style.visibility = 'visible';
-}
