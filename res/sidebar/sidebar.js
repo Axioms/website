3908150e246399a38d20f68e3b3993a168c2c8c7
Axioms <15842624+Axioms@users.noreply.github.com>
2018-07-18 01:07:44 -0400

    seamless iframes are a pain

--- res/sidebar/sidebar.js
+++ res/sidebar/sidebar.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     smallWindowSetUp();
 })
 function smallWindowSetUp() {
+    clean();
     if($(document).width() <= 768) {
         $(".sb-data").css({"height" : "0%"});
         $('div').removeAttr('style');
@@ -62,4 +63,22 @@ function flipArrow() {
 }
 function clean() {
     $('div').removeAttr('style');
+}
+function getDocHeight(doc) {
+    doc = doc || document;
+    // stackoverflow.com/questions/1145850/
+    var body = doc.body, html = doc.documentElement;
+    var height = Math.max( body.scrollHeight, body.offsetHeight, 
+        html.clientHeight, html.scrollHeight, html.offsetHeight );
+    return height;
+}
+function setIframeHeight(id) {
+    var ifrm = document.getElementById(id);
+    var doc = ifrm.contentDocument? ifrm.contentDocument: 
+        ifrm.contentWindow.document;
+    ifrm.style.visibility = 'hidden';
+    ifrm.style.height = "10px"; // reset to minimal height ...
+    // IE opt. for bing/msn needs a bit added or scrollbar appears
+    ifrm.style.height = getDocHeight( doc ) + 4 + "px";
+    ifrm.style.visibility = 'visible';
 }
