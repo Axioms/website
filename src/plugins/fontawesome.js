58cafd236bcd10e5430eb2d8c3e27b9c505ab7d4
Axioms <15842624+Axioms@users.noreply.github.com>
2019-05-16 08:45:12 -0400

    refactored font awesome
    Signed-off-by: Axioms <karan15547@gmail.com>

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faAndroid } from '@fortawesome/free-brands-svg-icons'
+import { faApple } from '@fortawesome/free-brands-svg-icons'
+import { faWindows } from '@fortawesome/free-brands-svg-icons'
+import { faChrome } from '@fortawesome/free-brands-svg-icons'
+import { faClipboard } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(fas)
+
+library.add(faAndroid, faApple, faWindows, faChrome, faClipboard)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
