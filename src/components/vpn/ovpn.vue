c8e5c35b4e4aa0f4b602a94e96111bdab66686ce
Axioms <15842624+Axioms@users.noreply.github.com>
2019-05-24 12:31:08 -0400

    moved ovpn html to it's own vue file

---
+++ src/components/vpn/ovpn.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="openvpn">
+        <div class="card"> 
+				<div class="card-header shadow rounded-top">
+					<h4 class="text-primary title d-inline-block">OpenVPN</h4>
+				</div>
+				<div class="card-body shadow bg-white rounded-bottom">
+					<div class="col-12">
+						<h3 class="display-4">OpenVPN</h3>
+						<p class="center">OpenVPN clients are given on demand and don't work on the school network.</p>
+						<p class="center">DM me if you want access.</p>
+					</div>
+				</div>
+			</div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
